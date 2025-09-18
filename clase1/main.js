63f4b2d301accfee7992601e47becedfce6a5b4f
Marjean17 <95670933+Marjean17@users.noreply.github.com>
2025-09-18 11:53:46 -0300

    Subo las practicas de las clases 1 y 2

---
+++ clase1/main.js
@@ -0,0 +1,16 @@
+//Clase 1
+let nombre = "Pirulo";
+let edad = 22;
+let antiguedad = 5;
+let curso = "JS";
+const PROFESION = "Analista programador"
+let cursoVacio;
+
+let presentacion = nombre + " " + PROFESION
+console.log(nombre + " grosso!")
+console.warn(nombre + " cuidado!")
+console.error(nombre + " la cagaste!")
+//alert("Alerta!!!")
+//let nuevo = prompt("daleee!") 
+//debugger
+//const resultado = confirm("Desea formatear el directorio C:?")
---
+++ clase2/main.js
@@ -0,0 +1,31 @@
+//debugger
+let nombre = "Pirul";
+console.log (typeof nombre)
+
+let edad = 22;
+let antiguedad = 5;
+let curso = "JS";
+const PROFESION = "Analista programador"
+let cursoVacio;
+
+let presentacion = nombre + " " + PROFESION
+
+if (nombre == "Pirulo") 
+{
+    console.log("Hola Pirulo!")
+} else if (nombre == "Virulo") {
+    console.log("mensaje para Virulo")
+}
+  else {console.warn(nombre, "🤷‍♂️ a vos no te conozco")}
+
+//-------------------
+let nro = parseInt("3") //typeof number
+let nro3 = ("3") //typeof srtring
+
+/*for (let i = 0; i <= 5; i++){
+    let n = prompt(" ingrese nombre nro " + i)
+    if (n == "c") {
+        continue
+    }
+    console.log(n + " tiene el turno " + i)
+}*/
